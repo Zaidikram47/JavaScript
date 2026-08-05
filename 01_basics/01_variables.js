6fca867f4a7c688f0dd89de961a4880d5c00175f
Zaid <zaidikram.g610s@gmail.com>
2026-08-05 05:05:12 +0000

    config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const userId = 1234;
+let userName = "Zaid Ikram";
+let userEmail = "zaidikram.g610s@gmail.com";
+let cityName = "Rahim Yar Khan";
+
+/*
+    use const for constant (unchangable) values
+    use let for variables (changable) values
+    dont use var as it has no scope despite it works as a variable but still prefer let
+*/
+
+console.table([userId,userName,userEmail,cityName]);
