91991bef3b8b8fd4d55806d25d79461b9438d639
Zaid <zaidikram.g610s@gmail.com>
2026-08-05 07:46:49 +0000

    changed

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+/*
+let score = 53;
+console.log(typeof score);
+score = "53";
+console.log(typeof score);
+ let value = Number(score);
+ console.log(typeof value);
+
+let isLoggedIn = true;
+console.log(typeof isLoggedIn);
+
+let boolisLoggedIn = Number(isLoggedIn, isLoggedIn);
+console.log(typeof boolisLoggedIn, boolisLoggedIn);
+
+
+let number = 47;
+let stringnumber = String(number);
+console.log("number is " + stringnumber + " and it is string");
+console.log("The proof is : " +stringnumber+ typeof(stringnumber));
+
+*/
+// Operations 
+
+let value = 4;
+let negvalue = -value;
+console.log(negvalue);
+
+
+let number = "1";
+console.log(typeof(number));
+
+
+console.log( 1 + 2 );
+console.log( "1" + 2 );
+console.log( 1 + 2 + "3" );
+console.log( "1" + 2 + 3 );
